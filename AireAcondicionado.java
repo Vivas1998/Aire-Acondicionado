418d86c12ca6126e47f1226f6e43053361641b0b
Pablo <pablovivasgarcia1@gmail.com>
2021-11-15 13:41:55 +0100

    0497.03 Minimo y maximo

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -9,27 +9,54 @@ public class AireAcondicionado
 {
     // instance variables - replace the example below with your own
     private double temperatura;
+    private double min;
+    private double max;
+    private double incremento;
 
     /**
      * Constructor for objects of class AireAcondicionado
      */
-    public AireAcondicionado()
+    public AireAcondicionado(double temperaturaMin, double temperaturaMax)
     {
         // initialise instance variables
         temperatura = 15.0;
+        incremento = 5.0;
+        min = temperaturaMin;
+        max = temperaturaMax;
     }
 
     //Subir temperatura
     public void subirTemperatura() {
-        temperatura = temperatura + 5.0;
+        if ((temperatura + incremento) <= max) {
+            temperatura = temperatura + incremento;
+        }
+        else {
+            System.out.println("Temperatura maxima alcanzada");
+        }
     }
     
     //Bajar Temperatura
     public void bajarTemperatura() {
-        temperatura = temperatura - 5.0;
+        if ((temperatura - incremento) >= min) {
+            temperatura = temperatura - incremento;
+        }
+        else {
+            System.out.println("Temperatura minima alcanzada");
+        }
     }
     
+    //Devuelve la temperatura
     public double getTemperatura() {
         return temperatura;
+    }
+    
+    //Indica el incremento
+    public void setIncremento(double aumentarIncremento) {
+        if (aumentarIncremento > 0) {
+            incremento = incremento + aumentarIncremento;
+        }
+        else {
+            System.out.println("No se permiten incrementos negativos");
+        }
     }
 }
